9e7fed179c577cb01a4c715ddb9174efba4953da
Matt Lannigan <matthew.lannigan@insignlanguage.co.uk>
2026-03-18 13:31:55 +0000

    add web.config
    Miss web.config for Azure window OS

--- index.js
+++ index.js
@@ -15,7 +15,6 @@
 // --------------- AZURE ------------------------------------
 const express = require("express");
 const path = require("path");
-
 const app = express();
 const port = process.env.PORT || 3000;
 
