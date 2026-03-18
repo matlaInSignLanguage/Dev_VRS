3ee7940fd8d4ce98a68c6213d4cdee93bfad743d
Matt Lannigan <matthew.lannigan@insignlanguage.co.uk>
2026-03-18 11:43:48 +0000

    fix fail run

--- index.js
+++ index.js
@@ -14,15 +14,17 @@
 
 // --------------- AZURE ------------------------------------
 const express = require("express");
-const app = express();
+const path = require("path");
 
-app.use(express.static("public"));
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) => {
-  res.send("App running");
+  res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
-const port = process.env.PORT || 3000;
 app.listen(port, () => {
-  console.log("Server running on port " + port);
+  console.log(`Server running on port ${port}`);
 });
