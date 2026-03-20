1564bde42c822d90b714e235a5ad3be61ef9bb77
Matt Lannigan <matthew.lannigan@insignlanguage.co.uk>
2026-03-20 10:03:09 +0000

    Add environment variables

---
+++ README.md
@@ -0,0 +1,16 @@
+List of environment variables
+
+SEO_DESCRIPTION =“book a video relay service appointment with InSignLanguage. Fast and easy online scheduling."
+GOOGLE_TAG=“<script async src="https://www.googletagmanager.com/gtag/js?id=G-BML8Y7794N"></script>
+  <script> window.dataLayer = window.dataLayer || []; function gtag() { dataLayer.push(arguments); } gtag('js', new Date()); gtag('config', 'G-BML8Y7794N'); </script>”
+CLARITY_TAG=“ <script type="text/javascript">
+    (function (c, l, a, r, i, t, y) {
+      c[a] = c[a] || function () { (c[a].q = c[a].q || []).push(arguments) };
+      t = l.createElement(r); t.async = 1; t.src = "https://www.clarity.ms/tag/" + i; y = l.getElementsByTagName(r)[0]; y.parentNode.insertBefore(t, y);
+    })(window, document, "clarity", "script", "u544aaafvy"); </script>”
+Home page
+HOME_TITLE = "welcome to InSignLanguage"
+HOME_PARAGRAPH = "Information about VRS..."
+Book page
+VRS_TITLE = "VRS book page"
+VRS_BOOK = "Microsoft book link"
--- index.js
+++ index.js
@@ -1,29 +1,22 @@
-//-------------- only work on localhost ------------------------------
-
-// const express = require('express');
-// const path = require('path');
-
-// const app = express();
-// const PORT = process.env.PORT || 3000;
-
-// app.use(express.static(path.join(__dirname, 'public')));
-
-// app.listen(PORT, () => {
-// console.log(`Server running at http://localhost:${PORT}`);
-// });
-
-// --------------- AZURE ------------------------------------
 const express = require("express");
 const path = require("path");
 const app = express();
+
 const port = process.env.PORT || 3000;
 
+// Serve static files
 app.use(express.static(path.join(__dirname, "public")));
 
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "index.html"));
+// API route to send multiple environment variables
+app.get("/api/message", (req, res) => {
+  res.json({
+    home_title: process.env.HOME_TITLE || "Welcome InSignLanguage",
+    home_paragraph: process.env.HOME_PARAGRAPH || "No paragraph found",
+    environment: process.env.NODE_ENV || "development"
+  });
 });
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
+  console.log(`http://localhost:${port}/`);
 });
