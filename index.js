0ceb010ec94e81d85f04f80dd9fff83228fb0641
Matt Lannigan <matthew.lannigan@insignlanguage.co.uk>
2026-03-23 13:37:46 +0000

    add web.config

--- index.js
+++ index.js
@@ -33,5 +33,6 @@ app.get("/api/message", (req, res) => {
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
+  console.log(`Environment: ${process.env.NODE_ENV || "development"}`);
   console.log(`http://localhost:${port}/`);
 });
