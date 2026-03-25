1a0ed82bf16e3dff99a4da05885b724224093913
Matt Lannigan <matthew.lannigan@insignlanguage.co.uk>
2026-03-25 13:41:59 +0000

    fix navbar and footer

--- public/layout.js
+++ public/layout.js
@@ -1,23 +1,23 @@
 function loadLayout() {
   document.getElementById("header").innerHTML = `
-    <header class="app-header">
-      <div class="brand">
-        <img src="images/ms-icon-150x150.png"
-             alt="InSignLanguage_logo"
-             class="brand-logo" />
-        <span class="brand-name">InSignLanguage</span>
-      </div>
-    </header>
-  `;
+  <header class="app-header">
+    <div class="header-inner">
+      <div class="left-group">
+        <a href="index.html" class="brand">
+          <img src="images/ms-icon-150x150.png"
+               alt="InSignLanguage_logo"
+               class="brand-logo" />
+          <span class="brand-name">InSignLanguage</span>
+        </a>
 
-  document.getElementById("sidebar").innerHTML = `
-    <aside class="app-sidebar">
-      <nav>
-        <a class="nav-item" href="index.html">Home</a>
-        <a class="nav-item" href="book.html">Book</a>
-      </nav>
-    </aside>
-  `;
+        <nav class="top-nav">
+          <a class="nav-item" href="index.html">Home</a>
+          <a class="nav-item" href="book.html">Book</a>
+        </nav>
+      </div>
+    </div>
+  </header>
+`;
 
   document.getElementById("footer").innerHTML = `
     <footer class="app-footer">
