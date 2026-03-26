f7c9389786c762b8d28200e339b9c8049c5d80c1
Matt Lannigan <matthew.lannigan@insignlanguage.co.uk>
2026-03-26 08:44:00 +0000

    change titile

--- public/layout.js
+++ public/layout.js
@@ -3,16 +3,10 @@ function loadLayout() {
   <header class="app-header">
     <div class="header-inner">
       <div class="left-group">
-        <a href="index.html" class="brand">
-          <img src="images/ms-icon-150x150.png"
-               alt="InSignLanguage_logo"
-               class="brand-logo" />
-          <span class="brand-name">InSignLanguage</span>
-        </a>
+      <span id="book_title" class="brand-name">InSignLanguage</span>
 
         <nav class="top-nav">
-          <a class="nav-item" href="index.html">Home</a>
-          <a class="nav-item" href="book.html">Book</a>
+          <a class="nav-item" href="book.html">Home</a>
         </nav>
       </div>
     </div>
