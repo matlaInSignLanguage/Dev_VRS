0a1ea61b9b0ded4db2a66de48770db4866b6ac49
Matt Lannigan <matthew.lannigan@insignlanguage.co.uk>
2026-03-27 10:56:35 +0000

    add hamburger button
    to allow view on mobile

--- public/layout.js
+++ public/layout.js
@@ -1,19 +1,23 @@
 function loadLayout() {
   document.getElementById("header").innerHTML = `
-  <header class="app-header">
-    <div class="header-inner">
-      <div class="left-group">
+<header class="app-header">
+  <div class="header-inner">
+    <div class="left-group">
       <span id="header_title" class="brand-name">InSignLanguage</span>
 
-        <nav class="top-nav">
-          <a class="nav-item" href="index.html">Home</a>
-          <a class="nav-item" href="book.html">Book VRS</a>
-          <a class="nav-item" href="join_call.html">Join a call</a>
-          <a class="nav-item" href="start_call.html">Start a call</a>
-        </nav>
-      </div>
+      <nav class="top-nav" id="topNav">
+        <a class="nav-item" href="index.html">Home</a>
+        <a class="nav-item" href="book.html">Book VRS</a>
+        <a class="nav-item" href="join_call.html">Join a call</a>
+        <a class="nav-item" href="start_call.html">Start a call</a>
+      </nav>
     </div>
-  </header>
+
+    <button class="menu-toggle" aria-label="Menu" aria-expanded="false">
+      ☰
+    </button>
+  </div>
+</header>
 `;
 
   document.getElementById("footer").innerHTML = `
@@ -23,4 +27,16 @@ function loadLayout() {
   `;
 
   document.getElementById("year").textContent = new Date().getFullYear();
+
+
+  const toggleBtn = document.querySelector('.menu-toggle');
+  const nav = document.querySelector('.top-nav');
+
+  toggleBtn.addEventListener('click', () => {
+    nav.classList.toggle('open');
+
+    const expanded = toggleBtn.getAttribute('aria-expanded') === 'true';
+    toggleBtn.setAttribute('aria-expanded', !expanded);
+  });
+
 }
