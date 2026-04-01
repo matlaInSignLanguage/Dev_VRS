2290173297aa05f06ae5404f8434043bc250899f
Matt Lannigan <matthew.lannigan@insignlanguage.co.uk>
2026-04-01 11:29:37 +0100

    Update layout.js

--- public/layout.js
+++ public/layout.js
@@ -3,7 +3,7 @@ function loadLayout() {
 <header class="app-header">
   <div class="header-inner">
     <div class="left-group">
-      <h1 id="header_title" class="brand-name">InSignLanguage</h1>
+    <h1 id="header_title" class="brand-name">InSignLanguage</h1>
 
       <nav class="top-nav" id="topNav">
         <a class="nav-item" href="index.html">Options</a>
